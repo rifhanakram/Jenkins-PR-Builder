d494e69715e81fd6ce68ae3e95c22a1760d32beb
Rifhan Akram <rifhan.akram1@gmail.com>
2016-10-25 16:30:19 +0530

    - testing jenkins

--- api/github-routes.js
+++ api/github-routes.js
@@ -4,15 +4,29 @@
  */
 "use strict";
 
+const GithubAPI = require('github');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-let temp;
-let data;
+
+let github = new GithubAPI({
+    debug : true,
+    protocol : 'https',
+    host : 'api.github.com',
+    pathPrefix : '/api/v3',
+    headers : {
+        'user-agent' : 'Jenkins-Builder'
+    },
+    Promise : require('blue-bird'),
+    followRedirects :  false,
+    timeout : 5000
+});
 
 module.exports = function(router){
     router.get('/auth',(req,res)=>{
-       temp = req;
-       //res.status(200).json({message:CLIENT_ID,status : "true"});
+       github.authenticate({
+           type : 'oauth',
+           
+       })
     });
 
     router.post('/event',(req,res) => {
@@ -21,7 +35,7 @@ module.exports = function(router){
     });
 
     router.get('/test', (req,res) => {
-        res.json({data:data,temp:temp});
+        res.json({data:CLIENT_ID,temp:CLIENT_SECRET});
     })
     return router;
 }
