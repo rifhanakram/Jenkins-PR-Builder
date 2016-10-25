0f1d5fc8a70badc20be3ab070b4930ef099f8e72
Rifhan Akram <rifhan.akram1@gmail.com>
2016-10-25 22:40:21 +0530

    - add file watcher

--- app.js
+++ app.js
@@ -5,10 +5,15 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const githubRoutes = require('./api/github-routes');
 const port = process.env.PORT || 3000;
 
 //app.use('*')
+fs.watch('/var/lib/jenkins/jobs',{ encoding : 'buffer'} , (evenType,filename) => {
+    console.dir(evenType);
+    console.dir(filename);
+});
 app.use('/api',githubRoutes(express.Router()));
 app.get('/',(req,res) => {
     res.status(200).json({message : "app is running"});
